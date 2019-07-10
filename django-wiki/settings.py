3d412951b45475f27670836e97978db84f9b2353
johanbook <johanbook@hotmail.com>
2019-07-10 21:15:41 +0200

    Rename blog app to wiki

--- django-wiki/settings.py
+++ django-wiki/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
+    'wiki.apps.WikiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

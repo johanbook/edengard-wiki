4a451dd0e1daec5cd78f261cf7ff0c948e141abf
JohanBook <johanbook@hotmail.com>
2019-08-01 18:18:07 +0200

    Remove previous settings

--- django-wiki/settings.py
+++ django-wiki/settings.py
@@ -125,9 +125,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "/wiki/static/wiki/"),
-]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 

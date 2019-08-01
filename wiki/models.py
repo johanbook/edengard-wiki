530d0df1bd41e9e91c4c291402159bc3ad201878
JohanBook <johanbook@hotmail.com>
2019-08-01 11:47:12 +0200

    Make articles load from database

--- wiki/models.py
+++ wiki/models.py
@@ -3,10 +3,11 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
-class Post(models.Model):
+class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
+    date_edited = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
--- wiki/views.py
+++ wiki/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 
+from .models import Article
+
 
 def about(request):
     return render(request, 'wiki/about.html')
 
 
 def home(request):
-    return render(request, 'wiki/home.html')
+    context = {
+        'article': Article.objects.first()
+    }
+    return render(request, 'wiki/home.html', context)
 
 
 

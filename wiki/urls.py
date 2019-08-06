5bd8257aba6201389ef50660f8a31380315fb7e1
JohanBook <johanbook@hotmail.com>
2019-08-06 17:34:58 +0200

    Add custom article page

--- wiki/urls.py
+++ wiki/urls.py
@@ -6,10 +6,12 @@ urlpatterns = [
     path("", views.home, name="wiki-home"),
     path("about/", views.about, name="wiki-about"),
     path("search/", views.SearchResultsView.as_view(), name="search"),
+
 ]
 
 for model in MODELS:
     name = model.__name__.lower()
-    urlpatterns.append(
-        path(f'{name}/', views.get_category_view(model), name=f'wiki-{name}')
-    )
+    urlpatterns.extend([
+        path(f'{name}/', views.get_category_view(model), name=f'wiki-{name}'),
+        path(f"{name}/<slug:title>", views.article_by_model(model), name=f'wiki-{name}-article'),
+    ])
